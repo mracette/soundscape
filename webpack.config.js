b0ccce06043c0b1921ba463e0b9d9908da94d1af
Mark Racette <markracette@gmail.com>
2019-08-20 20:15:45 -0400

    google fonts loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,71 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.mp3$/,
-                include: /src\/audio/,
-                loader: 'file-loader'
-            },
-            { 
-                test: /\.js$/, 
-                exclude: /node_modules/, 
-                loader: "babel-loader" 
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.mp3$/,
+                    include: path.join(__dirname, 'src/audio/'),
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'audio',
+                        name: '[name].[ext]'
+                    }
+                },
+                { 
+                    test: /\.js$/, 
+                    exclude: /node_modules/, 
+                    loader: "babel-loader" 
+                },                
+                {
+                    test: /\.s?css$/,
+                    use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                            name: '[name].[ext]'
+                        }
+                    },
                     'css-loader',
                     'sass-loader'
-                ]
-            }
+                    ]
+                },
+                {
+                    test: /\.gltf$/,
+                    include: path.join(__dirname, 'src/models/'),
+                    loader: "file-loader",
+                    options: {
+                        outputPath: 'models',
+                        name: '[name].[ext]'
+                    }
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }
+            ]
+        },
+        devtool: isProduction ? 'source-map': 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css',
+                chinkFilename: 'styles.map.css'
+            })
         ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    },
-}
+}}
