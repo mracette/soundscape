ee336d4688c178a1738bcedf4e0cd7a03141bc66
Mark Racette <markracette@gmail.com>
2019-08-21 09:19:41 -0400

    layout changes

--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -4,9 +4,6 @@ import { Link } from 'react-router-dom';
 export default class NavigationPanel extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-
-        }
     }
     render() {
         return (
--- src/components/Sliders.js
+++ src/components/Sliders.js
@@ -15,21 +15,21 @@ export default class Sliders extends React.Component {
     render() {
         return (
             <div>
-                <h3>highpass filter</h3>
+                <h3 className='slider-label'>highpass filter</h3>
                 <input type="range" min="1" max="100" className="slider" id="hp-slider"
                     onInput = {(e) => {
                         e.preventDefault();
                         this.props.handleChangeHP(e.target.value);
                     }}
                 ></input>
-                <h3>lowpass filter</h3>
+                <h3 className='slider-label'>lowpass filter</h3>
                 <input type="range" min="1" max="100" className="slider" id="lp-slider"
                     onInput = {(e) => {
                         e.preventDefault();
                         this.props.handleChangeLP(e.target.value);
                     }}
                 ></input>
-                <h3>ambience</h3>
+                <h3 className='slider-label'>ambience</h3>
                 <input type="range" min="1" max="100" className="slider" id="ambience-slider"
                     onInput = {(e) => {
                         e.preventDefault();
