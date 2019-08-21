7fe80ad5f967e4799ae266f0690d2819cd2a4f9f
Mark Racette <markracette@gmail.com>
2019-08-21 09:18:30 -0400

    user gesture to resume context

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import AppRouter from '../routers/AppRouter';
 import MusicPlayer from '../components/MusicPlayer';
+import SongLandingPage from '../components/SongLandingPage';
 
 export default class AppContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            config: require('../app-config.json')
+            config: require('../app-config.json'),
+            userGesture: false
         }
+        this.handleUpdateUserGesture = this.handleUpdateUserGesture.bind(this);
     }
+
+    handleUpdateUserGesture(bool) {
+        // the webaudio context won't start unless there has been
+        // a user gesture detected on the page first, so keep track
+        // of that (and update it) here.
+        this.setState(() => ({userGesture: bool}));
+    }
+
     render() {
         return (
-            <div>
+            <div className = 'app-container'>
                 {/* <AppRouter 
                     config = {this.state.config}
                 /> */}
+                <SongLandingPage 
+                    handleUpdateUserGesture = {this.handleUpdateUserGesture}
+                    userGesture = {this.state.userGesture}
+                />
                 <MusicPlayer
                     songConfig = {this.state.config[0]}
+                    userGesture = {this.state.userGesture}
                 />
             </div>
         );
