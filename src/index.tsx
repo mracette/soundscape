87f038af8c70621d12f58b2ce25d8d288b1ca535
mracette <markracette@gmail.com>
2026-06-28 08:14:43 -0500

    feat(mobile): host-gate Google Analytics to web only

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App";
 import { hardenAppShell } from "./utils/appShell";
+import { initAnalytics } from "./utils/analytics";
 
 hardenAppShell();
+initAnalytics();
 
 const root = createRoot(document.getElementById("app")!);
 root.render(<App />);
