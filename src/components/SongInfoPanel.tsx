f78f9bbcb49af4417f6f4cbb07dffc4fcb90d391
mracette <markracette@gmail.com>
2026-07-03 17:36:20 -0500

    Tokenize SongInfoPanel credit color, minor spacing

--- src/components/SongInfoPanel.tsx
+++ src/components/SongInfoPanel.tsx
@@ -3,7 +3,10 @@ import { useContext } from "react";
 import { SongContext } from "../contexts/contexts";
 import { InfoContext } from "../contexts/contexts";
 
-import { songInfoPanel } from "../styles/components/SongInfoPanel.css";
+import {
+  creditType,
+  songInfoPanel,
+} from "../styles/components/SongInfoPanel.css";
 
 export const SongInfoPanel = () => {
   const { name } = useContext(SongContext)!;
@@ -15,7 +18,7 @@ export const SongInfoPanel = () => {
       {credits.map((c) => {
         return (
           <p key={c.type}>
-            <span style={{ color: "rgb(0, 225, 158)" }}>{c.type}</span>
+            <span className={creditType}>{c.type}</span>
             &nbsp;
             {c.link ? <a href={c.link}>{c.content}</a> : c.content}
           </p>
--- src/styles/components/SongInfoPanel.css.ts
+++ src/styles/components/SongInfoPanel.css.ts
@@ -1,10 +1,21 @@
 import { globalStyle, style } from "@vanilla-extract/css";
 
+import { accents, textPrimary, vh } from "../settings";
+
 export const songInfoPanel = style({
-  color: "white",
+  color: textPrimary,
   display: "flex",
   flexDirection: "column",
   flexWrap: "nowrap",
+});
+
+globalStyle(`${songInfoPanel} h2`, {
+  marginBottom: vh(1),
+});
+
+export const creditType = style({
+  color: accents.moss.base,
+  fontWeight: 500,
 });
 
 globalStyle(`${songInfoPanel} p`, {
